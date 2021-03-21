05811dc124285ba325ffc1cdf5c47cc4d1d4e44e
Daria Radu <dariaradur3@gmail.com>
2021-03-21 17:05:46 +0100

    Prepare for initial deploy to firebase

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './app.css'
+import logo from './logo.png'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Daria's website!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
